01f1eba25e495143931c2b8ced5f90e61df57ae5
Peter McDonald <me@petermcdonald.co.uk>
2022-09-04 16:09:08 +0100

    Initial Release

---
+++ monzo_viewer/cli.py
@@ -0,0 +1,21 @@
+"""Entry point for Monzo Viewer module."""
+from monzo_viewer.app import app
+
+
+def start(local_only: bool = True, port: int = 5000, debug: bool = False):
+    """
+    Entry point for Monzo Viewer.
+
+    Args:
+        local_only: Only listen to local interface if True otherwise listen to all
+        port: Port to list on
+        debug: Start in debugging mode if True
+    """
+    host: str = '127.0.0.1'
+    if not local_only:
+        host = '0.0.0.0'
+    app.run(host=host, port=port, debug=debug)
+
+
+if __name__ == '__main__':
+    start()
